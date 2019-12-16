0aeee2f0b123314b4c719cbf1b55361f6550dd62
Benjamin Small <ben20262@aim.com>
2019-12-16 23:49:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each {|word| puts word}
+  end
+
+  def save
+    @@all << self.name
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
